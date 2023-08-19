18fb31b8633cdee4f9cccbc0e7aede0cdc1aa3d7
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-08-19 12:07:23 -0700

    calculateHash function

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package simpleBlockchain
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+var Blockchain []Block
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	hash := sha256.New()
+	hash.Write([]byte(record))
+	hashed := hash.Sum(nil)
+
+	return hex.EncodeToString(hashed)
+}
